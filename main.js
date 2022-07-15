643da5af18d32a4361464a374229f11a2bd68f4b
RitwikChatterjee123456788 <102661739+RitwikChatterjee123456788@users.noreply.github.com>
2022-07-15 15:38:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name="Ritwik"
+var second_name="Chatterjee"
+var full_name=first_name.concat(second_name);
+console.log (full_name);
+
+var number1="15"
+console.log (number1);
+
+var number2="5"
+console.log (number2);
+
+var number3="50"
+console.log (number3);
+
+var number4="2"
+console.log (number4)
